6d70e4769cb106083e1b1ee66c59316b816f718c
Wendel <senshi13wendel@gmail.com>
2024-05-16 21:25:25 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form')
+
+function validaNome(nomeTodo){
+  const nomeComoArray = nomeTodo.split(' ');
+  return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  const nomeCompleto = document.getElementById('nomeCompleto');
+  if (!validaNome(nomeCompleto.value)) {
+    alert('Preencha o nome completo')
+  } else{
+    alert('Formulário correto!')
+  }
+})
